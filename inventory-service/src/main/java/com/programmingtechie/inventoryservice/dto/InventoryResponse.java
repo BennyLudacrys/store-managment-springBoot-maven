dcd68007571c13a1785fd2251dfc4b9cf9ab85ab
bennedito01@gmail.com <bennedito01@gmail.com>
2022-12-27 22:30:47 +0200

    integracao dos microservicos inventory e order

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/dto/InventoryResponse.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/dto/InventoryResponse.java
@@ -1,2 +1,15 @@
-package com.programmingtechie.inventoryservice.dto;public class InventoryResponse {
+package com.programmingtechie.inventoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryResponse {
+    private String skuCode;
+    private boolean isInStock;
 }
--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,10 @@ package com.programmingtechie.inventoryservice.repository;
 import com.programmingtechie.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
