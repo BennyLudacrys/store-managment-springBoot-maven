f37d39b444006c413986d82750159c6110aef9f7
bennedito01@gmail.com <bennedito01@gmail.com>
2023-01-06 12:00:16 +0200

    implementacao do apigateway

---
+++ discover-server/src/main/java/com/bennyTech/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,2 @@
+package com.bennyTech.discoveryserver;public class DiscoveryServerApplication {
+}
--- discover-server/src/main/java/com/programming/techie/DiscoveryServerApplication.java
+++
@@ -1,9 +0,0 @@
-package com.programming.techie;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class DiscoveryServerApplication {
-    public static void main(String[] args) {
-       SpringBootApplication.run(DiscoveryServerApplication.class, args);
-    }
-}
